07b5e7d76466cee19705174d5c535138105d5e34
Todd Wolfson <todd@twolfson.com>
2013-05-23 00:06:40 -0700

    Adding notes for future dev

--- test/grunt.js
+++ test/grunt.js
@@ -19,6 +19,16 @@ module.exports = function (grunt) {
         //   'woff': 'actual_files/font.waffles',
         //   'eot': 'actual_files/more.like.eof'
         // }
+
+        // TODO: Custom routing of font filepaths for CSS intricacies
+        // cssPathRouter: function (fontpath) {
+        //   return fontpath;
+        // }
+
+        // TODO: Allow custom naming of font families for multi support
+        // TODO: It would be smart to name it after the task ;)
+        // TODO: Don't forget to escape this (JSON.stringify)
+        // fontname: 'my-icon-font'
       }
     }
   });
