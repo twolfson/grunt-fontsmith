4c61092032f11b51cd4b34b579dbf8480d8dd31d
Todd Wolfson <todd@twolfson.com>
2013-05-28 13:36:38 -0700

    Attempting to form better test sentences

--- test/grunt-fontsmith_outline.js
+++ test/grunt-fontsmith_outline.js
@@ -1,14 +1,16 @@
 module.exports = {
-  'grunt-fontsmith generating a single font and a single stylesheet': {
-    'produces a stylesheet': true,
-    'produces a font': true
-  },
-  'grunt-fontsmith generating multiple fonts and stylesheets': {
-    'produces multiple stylesheets': true,
-    'produces multiple fonts': true
-  },
-  'grunt-fontsmith generating overridden fonts and stylesheets': {
-    'produces stylesheets with proper formats': true,
-    'produces fonts with proper forms': true
+  'A set of SVGs': {
+    'processed into a single font and a single stylesheet': {
+      'produces a stylesheet': true,
+      'produces a font': true
+    },
+    'processed into multiple fonts and stylesheets': {
+      'produces multiple stylesheets': true,
+      'produces multiple fonts': true
+    },
+    'processed into overridden fonts and stylesheets': {
+      'produces stylesheets with proper formats': true,
+      'produces fonts with proper forms': true
+    }
   }
 };
