cecc8008675e5347511ba1304dbafbe6ede73b98
Todd Wolfson <todd@twolfson.com>
2013-06-09 23:54:02 -0700

    Successfully screenshotting webpage

--- test/test_scripts/screenshot_font.js
+++ test/test_scripts/screenshot_font.js
@@ -1,7 +1,22 @@
-// Grab the font we are screenshotting
+// Load in dependencies
+var webpage = require('webpage'),
+    __dirname = phantom.libraryPath;
+
+// Grab the path of CSS to load
 var args = phantom.args,
     cssPath = args[0];
 
-console.log(cssPath);
+// Open a webpage with our CSS
+var page = webpage.create(),
+    __dirname =
+page.open(__dirname + '/screenshot_font.html', function (status) {
+  console.log(status);
+  // Add in characters showcasing our font
 
-phantom.exit(0);
+  // Take a screenshot of the content
+  var screenshot = page.renderBase64('png');
+  console.log(screenshot, 'yy');
+
+  // Exit the program
+  phantom.exit(0);
+});
