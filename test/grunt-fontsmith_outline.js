e1aa1fedd681c957e9cf5a16438178c978b43ec0
Todd Wolfson <todd@twolfson.com>
2014-02-08 15:52:44 -0800

    Upgraded doubleshot and test outline

--- test/grunt-fontsmith_outline.js
+++ test/grunt-fontsmith_outline.js
@@ -1,16 +1,18 @@
-module.exports = {
-  'A set of SVGs': {
-    'processed into a single font and stylesheet': {
-      'produces a stylesheet': true,
-      'produces a font': true
-    },
-    'processed into multiple fonts and stylesheets': {
-      'produces multiple stylesheets': true,
-      'produces multiple fonts': true
-    },
-    'processed into overridden fonts and stylesheets': {
-      'produces stylesheets with proper formats': true,
-      'produces fonts with proper formats': true
-    }
-  }
-};
+module.exports = [{
+  'A set of SVGs': [{
+    'processed into a single font and stylesheet': [
+      'produces a stylesheet',
+      'produces a font'
+    ]
+  }, {
+    'processed into multiple fonts and stylesheets': [
+      'produces multiple stylesheets',
+      'produces multiple fonts'
+    ]
+  }, {
+    'processed into overridden fonts and stylesheets': [
+      'produces stylesheets with proper formats',
+      'produces fonts with proper formats'
+    ]
+  }]
+}];
