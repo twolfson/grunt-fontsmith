af47a8d4a7206ab28bc6ee582e1ce63c6b44fbd7
Todd Wolfson <todd@twolfson.com>
2014-03-03 03:43:51 -0800

    Transferring over Stylus rendering

--- test/grunt-fontsmith_test.js
+++ test/grunt-fontsmith_test.js
@@ -1,5 +1,7 @@
+var fs = require('fs');
 var expect = require('chai').expect;
 var fsUtils = require('./utils/fs');
+var imageUtils = require('./utils/image');
 
 function runGruntTask(task) {
   before(function (done) {
@@ -38,10 +40,64 @@ describe('A set of SVGs', function () {
     // runGruntTask('font:single');
     fsUtils.loadActualLines(__dirname + '/actual_files/single/font.styl');
     fsUtils.loadExpectedLines(__dirname + '/expected_files/single/font.styl');
-    // screenshotUtils.saveActualFont({
+
+    // imageUtils.screenshotActualFont({
     //   path: 'single/font.svg',
     //   format: 'svg'
     // });
+    before(function generateStylus (done) {
+      // Define and load Stylus to work with
+      var expectedCssObj = {
+        eot: fs.readFileSync(__dirname + '/test_files/font.eot.css', 'utf8'),
+        svg: fs.readFileSync(__dirname + '/test_files/font.svg.css', 'utf8'),
+        ttf: fs.readFileSync(__dirname + '/test_files/font.ttf.css', 'utf8'),
+        woff: fs.readFileSync(__dirname + '/test_files/font.woff.css', 'utf8')
+      };
+      var charStyl = [
+        '.icon-eye',
+        '  icon($eye)',
+        '.icon-building_block',
+        '  icon($building_block)',
+        '.icon-moon',
+        '  icon($moon)'
+      ].join('\n');
+      var actualStyl = fs.readFileSync(__dirname + '/actual_files/single/font.styl', 'utf8');
+
+      // Remove unused font formats
+      // TODO: Use `screenshotActualFont.format`
+      var fontFormat = 'svg';
+      if (fontFormat !== 'eot') {
+        actualStyl = actualStyl.replace(/\s+src:url\("font.eot"\);/, '');
+        actualStyl = actualStyl.replace(/\s*url\("font.eot\?#iefix"\) format\("embedded-opentype"\),\s*/, '');
+      }
+      if (fontFormat !== 'woff') {
+        actualStyl = actualStyl.replace(/\s*url\("font.woff"\) format\("woff"\),\s*/, '');
+      }
+      if (fontFormat !== 'ttf') {
+        actualStyl = actualStyl.replace(/\s*url\("font.ttf"\) format\("truetype"\),\s*/, '');
+      }
+      if (fontFormat !== 'svg') {
+        // Guarantee no-commas for font formats
+        actualStyl = actualStyl.replace(',', ';');
+        actualStyl = actualStyl.replace(/\s*url\("font.svg#icomoon"\) format\("svg"\);\s*/, '');
+      }
+
+      // Replace font path with our font path
+      // TODO: Replace with `screenshotActualFont.path`
+      var actualDir = __dirname + '/actual_files';
+      var filepath = 'single/font.svg';
+      var fontname = 'font.' + fontFormat;
+      var expectedCss = expectedCssObj[fontFormat];
+      actualStyl = actualStyl.replace(fontname,  actualDir + filepath);
+
+      // Assert our replacements were successful
+      expect(actualStyl).to.contain(actualDir, 'Actual stylus has not replaced "' + fontname + '" with "' + actualDir + filepath + '" successfully');
+
+      // TODO: Perform in separate screenshot action
+      // var expectedDir = __dirname + '/expected_files';
+      // expectedCss = expectedCss.replace(fontname, expectedDir + filepath);
+      // expect(expectedCss).to.contain(expectedDir, 'Expected css has not replaced "' + fontname + '" with "' + expectedDir + filepath + '" successfully');
+    });
       // terfall([
       //       stylus.render.bind(this, actualStyl + '\n' + charStyl),
       //       saveToFile,
@@ -49,6 +105,7 @@ describe('A set of SVGs', function () {
       //     ], cb);
       //       // Screenshot the font in use
       // console.log(cssPath);
+
       // exec('phantomjs test_scripts/screenshot_font.js ' + cssPath, function (err, stdout, stderr) {
       //   // Fallback error with stderr
       //   if (!err && stderr) {
